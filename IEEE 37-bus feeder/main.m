6639f8cdcb1e3bfe82080c7fbb277a64e70268fb
Hafez Bazrafshan <Hafez@Hafezs-MacBook-Pro.local>
2017-07-03 17:15:47 -0500

    some random tests

--- IEEE 37-bus feeder/main.m
+++ IEEE 37-bus feeder/main.m
@@ -2,7 +2,7 @@
 clear all;
 clc;
 
-IEEE37 = setupYbusIEEE37('non-ideal',1e-9 );
+IEEE37 = setupYbusIEEE37('non-ideal',1e-6 );
 IEEE37=computeNoLoadVoltage(IEEE37, [1;1;1], [0; -120; 120]);
  IEEE37  = setupLoadsIEEE37( IEEE37 );
 IEEE37= performZBus(IEEE37); 
