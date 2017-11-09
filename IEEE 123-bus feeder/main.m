7d1dbceec367f651546353f7316fe9209e018aae
Hafez Bazrafshan <Hafez@Hafezs-MacBook-Pro.local>
2017-11-08 20:12:38 -0600

    a re-run to double check

--- IEEE 123-bus feeder/main.m
+++ IEEE 123-bus feeder/main.m
@@ -7,6 +7,6 @@ IEEE123=computeNoLoadVoltage(IEEE123, [1;1;1], [0; -120; 120]);
  IEEE123  = setupLoadsIEEE123( IEEE123 );
 IEEE123= performZBus(IEEE123); 
 IEEE123= obtainVoltages(IEEE123); 
-% IEEE123=obtainDSSVoltages(IEEE123);
+IEEE123=obtainDSSVoltages(IEEE123);
 IEEE123=obtainKerstingVoltages(IEEE123);
 plotVoltages(IEEE123 )
